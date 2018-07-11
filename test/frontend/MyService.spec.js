9e1ed2e430816585ca590a4845822455b7caf3c4
Dominik Paluszak <dominik.paluszak@kinguin.net>
2018-07-11 12:07:59 +0200

    added mongo

--- test/frontend/MyService.spec.js
+++
@@ -1,8 +0,0 @@
-console.log('this is a test for MyService')
-
-describe('hello', function () {
-  console.log('in describe')
-  it('should do stuff', function () {
-    expect(1 + 1).toBe(2)
-  })
-})
